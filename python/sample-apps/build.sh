d8088f5581a0e13f8e29536b3a6abe7197476e2c
Robert J. Berger <rob@omnyway.com>
2022-08-06 22:27:14 -0700

    This seems to work but without aws xray and starutp

--- python/sample-apps/build.sh
+++ python/sample-apps/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+echo "------- sampel-apps build -------"
 mkdir -p build/python
 python3 -m pip install -r function/requirements.txt -t build/python
-cp function/lambda_function.py -t build/python
+cp function/lambda_function.py build/python
 cd build/python || exit
 zip -r ../function.zip *
